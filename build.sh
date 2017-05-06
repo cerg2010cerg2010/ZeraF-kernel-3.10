3f1070d9f7949040ac9e05e4e60ecaaf7c882201
cerg2010cerg2010 <cerg2010cerg2010@mail.ru>
2017-05-06 20:26:56 +0300

    GCC 6 build support

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-env CROSS_COMPILE=/opt/android/toolchains/arm-eabi-4.7/bin/arm-eabi- ARCH=arm SUBARCH=arm make -j`nproc`
+#env CROSS_COMPILE=/opt/android/toolchains/arm-eabi-4.7/bin/arm-eabi- ARCH=arm SUBARCH=arm make -j`nproc` $@
+env CROSS_COMPILE=/opt/android/toolchains/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- ARCH=arm SUBARCH=arm make -j`nproc` $@
